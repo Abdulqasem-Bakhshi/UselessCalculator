1429d223cbf7d3c942eb9e58e7a9b96857eb327a
Abdulqasem-Bakhshi <q.bakhshi@outlook.com>
2025-03-26 17:36:23 +0100

    last attempt push

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 
 let mainWindow;
 
+function createWindow() {
+  if (mainWindow) {
+    return;
+  }
+
+}
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 300,
-    height: 411,
+    height: 359,
     frame: false,
     resizable: false,
     icon: __dirname + '/icon.png',
     webPreferences: {
+      devTools: false,
       nodeIntegration: true,
-      contextIsolation: false, // Required for ipcRenderer to work
+      contextIsolation: false,
     },
   });
 
@@ -31,8 +38,29 @@ ipcMain.on('close-window', () => {
   }
 });
 
+// Handle the single instance lock
+const gotLock = app.requestSingleInstanceLock();
+
+if (!gotLock) {
+  // If another instance is running, quit the app
+  app.quit();
+} else {
+  app.whenReady().then(() => {
+    // Create the main window
+    createWindow();
+
+    // This will handle the behavior when another instance tries to start
+    app.on('second-instance', (event, argv, cwd) => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+          mainWindow.restore();
+        }
+        mainWindow.focus();
+      }
+    });
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
-});
+})})};
--- renderer.js
+++ renderer.js
@@ -1,12 +1,18 @@
 const { ipcRenderer } = require('electron');
 
+window.onload = () => {
+
+// Close and Minimize buttons
 const minimizeButton = document.getElementById('minimize');
 const closeButton = document.getElementById('close');
 
+// Minimize window
 minimizeButton.addEventListener('click', () => {
-  ipcRenderer.send('minimize-window'); // Send a message to the main process
+  ipcRenderer.send('minimize-window');
 });
 
+// Close window
 closeButton.addEventListener('click', () => {
-  ipcRenderer.send('close-window'); // Send a message to the main process
+  ipcRenderer.send('close-window');
 });
+};
